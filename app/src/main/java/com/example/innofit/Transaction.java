f581f75b33e8d4f5878e979bbb8bf013618acac2
Eric <mjereginaldo@tip.edu.ph>
2023-05-09 19:26:59 +0800

    ###updated transaction

--- app/src/main/java/com/example/innofit/Transaction.java
+++ app/src/main/java/com/example/innofit/Transaction.java
@@ -5,16 +5,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Transaction extends AppCompatActivity {
+
+    private Button trainer1, trainer2, trainer3, trainer4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction);
 
+        trainer1 = (Button) findViewById(R.id.trainer1btn);
+
+        trainer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Transaction.this, Trainer1.class);
+                startActivity(intent);
+
+            }
+        });
+        trainer2 = (Button) findViewById(R.id.trainer2btn);
+        trainer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Transaction.this, Trainer2.class);
+                startActivity(intent);
+
+            }
+        });
+        trainer3 = (Button) findViewById(R.id.trainer3btn);
+        trainer3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Transaction.this, Trainer3.class);
+                startActivity(intent);
+
+            }
+        });
+        trainer4 = (Button) findViewById(R.id.trainer4btn);
+        trainer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Transaction.this, Trainer4.class);
+                startActivity(intent);
+
+            }
+        });
+
         ImageButton disc = (ImageButton) findViewById(R.id.discs);
+
         disc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -51,6 +93,7 @@ public class Transaction extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 }
 

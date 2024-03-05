e539a84eb9fc942c0174bb0a32cada62a166faa9
Jhon-Eric <mjereginaldo@tip.edu.ph>
2024-03-05 23:46:32 +0800

    backbutton ericwashere

--- app/src/main/java/com/example/innofit/Signup.java
+++ app/src/main/java/com/example/innofit/Signup.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
@@ -18,6 +19,7 @@ public class Signup extends AppCompatActivity {
 
 
      Button login, signup;
+     private ImageView image;
      EditText nametxt, usertxt, emailtxt, passtxt, heighttxt, weighttxt;
      String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
@@ -32,6 +34,7 @@ public class Signup extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         //hooks for sign_up
+        image= findViewById(R.id.signup_back_button);
         signup = (Button) findViewById(R.id.signup_btn);
         login = (Button) findViewById(R.id.login_btn);
         nametxt = (EditText) findViewById(R.id.nametxt);
@@ -43,6 +46,15 @@ public class Signup extends AppCompatActivity {
 
 
         FirebaseApp.initializeApp(this);
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Signup.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
         signup.setOnClickListener(new View.OnClickListener() {

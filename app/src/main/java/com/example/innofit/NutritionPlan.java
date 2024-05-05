c5b64f616f692335c547721f76fa974c31a1a94f
eric <mjereginaldo@tip.edu.ph>
2024-05-05 20:19:24 +0800

    meal plan progress
    -added content on nutrition plan
    -new navbar added and working
    -added imgbtns for overweight and underweight nutrition plan
    -added header

--- app/src/main/java/com/example/innofit/NutritionPlan.java
+++ app/src/main/java/com/example/innofit/NutritionPlan.java
@@ -1,6 +1,9 @@
 package com.example.innofit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +11,62 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class NutritionPlan extends AppCompatActivity {
 
+    ImageButton dashboard, todolist, nutrition, users;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_nutrition_plan);
 
+        dashboard = (ImageButton) findViewById(R.id.dashboard);
+        todolist = (ImageButton) findViewById(R.id.todolist);
+        nutrition = (ImageButton) findViewById(R.id.nutrition);
+
+
+        // start of intents in navbar
+        dashboard.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            openDashboard (view);
+        }
+
+        private void openDashboard(View view) {
+            Intent intent = new Intent(NutritionPlan.this, StepCounter.class);
+            startActivity(intent);
+        }
+    });
+
+        todolist.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            openTodolist(view);
+        }
+
+
+        private void openTodolist(View view) {
+            Intent intent = new Intent(NutritionPlan.this, ProgressTrackerActivity.class);
+            startActivity(intent);
+        }
+    });
+
+        nutrition.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            openNutrition(view);
+        }
+
+
+        private void openNutrition(View view) {
+            Intent intent = new Intent(NutritionPlan.this, NutritionPlan.class);
+            startActivity(intent);
+        }
+    });
     }
-}
+
+
+    }
+
+

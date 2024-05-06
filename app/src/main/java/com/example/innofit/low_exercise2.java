5ea81216e50a36ccf8306919cccf33601a99acf5
Jerome <carsolajj@gmail.com>
2024-05-06 23:15:08 +0800

    Profile Layout
    Tapos na yung user profile nalang nna nasa taas papalitan ko

--- app/src/main/java/com/example/innofit/low_exercise2.java
+++ app/src/main/java/com/example/innofit/low_exercise2.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class low_exercise2 extends AppCompatActivity {
+public class Low_exercise2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
